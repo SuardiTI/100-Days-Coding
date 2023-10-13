36a9b7ede5e221565fc3b7c2a4d35639cb411578
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-10-13 19:18:35 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,11 @@
+package Day5;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int hari1 = 5;
+        int hari2 = 100;
+        System.out.println("Sisa Hari : "+(hari2-hari1)+' '+"Hari");
+    }
+    
+}
