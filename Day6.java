432fb98efe68705987320dc3d9f4db2a91586b8f
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-10-14 17:00:26 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,13 @@
+package Day6;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        byte angka1,angka2;
+        
+        angka1 = 7;
+        angka2 = 4;
+        System.out.println(angka1 <= angka2);
+    }
+    
+}
