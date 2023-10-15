d8fc2e4d97eb4399719b8d6b6e381c41781c6380
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-10-15 10:02:37 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,13 @@
+package Day7;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int angka = 12;
+        int nilai = 10;
+        
+        System.out.println(angka+nilai);
+    }
+    
+}
