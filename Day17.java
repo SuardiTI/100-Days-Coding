baa576a818f2f77c0106e0154d5cd9a23987b027
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-10-25 18:11:43 +0800

    Day17.java
    Belajar Konversi Tipe Data String Ke Tipe Data Primitif (Ke Tipe Data Integer)

---
+++ Day17.java
@@ -0,0 +1,12 @@
+package Day17;
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //Konversi Tipe Data String Ke Tipe Data Primitif (Ke Tipe Data Integer)
+        String StrAngka = "100";
+        int angka = Integer.parseInt(StrAngka);
+        System.out.println(angka);
+    }
+    
+}
