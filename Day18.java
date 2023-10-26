3056a8a3026e4daf571de55279a757dc1f42c454
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-10-26 15:50:25 +0800

    Day18.java
    Mengonversi Tipe Data String Ke Tipe Data Primitif (Ke Tipe Data Double)

---
+++ Day18.java
@@ -0,0 +1,13 @@
+package Day18;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Mengonversi Tipe Data String Ke Tipe Data Primitif (Ke Tipe Data Double)
+        String StrAngka = "50.2";
+        double angka = Double.parseDouble(StrAngka);
+        
+        System.out.println(angka);
+    }
+    
+}
