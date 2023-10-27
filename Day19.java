52177478b86f5649cf63f7fb2e27499b0e2e31e0
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-10-27 17:49:46 +0800

    Day19.java
    Membuat Perulangan Untuk Mencetak Angka Dan Nama Sebanyak 10x

---
+++ Day19.java
@@ -0,0 +1,11 @@
+package Day19;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Soal : Membuat Perulangan Untuk Mencetak Angka Dan Nama Sebanyak 10x
+        for (int a=0; a<10; a++)
+        System.out.println(a+1+". Suardi");
+    } 
+    
+}
