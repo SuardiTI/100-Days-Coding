582b9c837f17dcfc0c501288601f2d32ea71c1c9
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-10-28 14:13:21 +0800

    Day20.java
    Mengonversi Tipe Data String Ke Tipe Data Primitif (Ke Tipe Data Long)

---
+++ Day20.java
@@ -0,0 +1,12 @@
+package Day20;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Mengonversi Tipe Data String Ke Tipe Data Primitif (Ke Tipe Data Long)
+        String StrAngka = "1234567890";
+        long angka = Long.parseLong(StrAngka);
+        System.out.println(angka);
+    }
+    
+}
