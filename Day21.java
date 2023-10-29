b78bda696039ac2d3daf552052ce4d530363476a
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-10-29 11:08:42 +0800

    Day21.java
    Mengonversi Tipe Data String Ke Tipe Data Primitif (Ke Tipe Data Float)

---
+++ Day21.java
@@ -0,0 +1,14 @@
+package Day21;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //Mengonversi Tipe Data String Ke Tipe Data Primitif (Ke Tipe Data Float)
+        String StrAngka = "150.25";
+        float angka = Float.parseFloat(StrAngka);
+        
+        System.out.println(angka);
+    }
+    
+}
