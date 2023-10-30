6667982d2823271b7356e36e3c509e671384466e
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-10-30 19:13:45 +0800

    Day22.java
    Konversi Tipe Data String Ke Tipe Data Primitif (Ke Tipe Data Byte)

---
+++ Day22.java
@@ -0,0 +1,13 @@
+package Day22;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Konversi Tipe Data String Ke Tipe Data Primitif (Ke Tipe Data Byte)
+        String StrAngka = "22";
+        byte angka = Byte.parseByte(StrAngka);
+        
+        System.out.println(angka);
+    }
+    
+}
