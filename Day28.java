bc70a3512c9df9ff2964eb9f0e4200fbdb60a356
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-05 21:47:58 +0800

    Belajar Operator Penugasan

---
+++ Day28.java
@@ -0,0 +1,15 @@
+package Day28.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //belajar operator penugasan
+        int angka = 10;
+        angka*= 2;
+        angka-= 4;
+        angka/= 2;
+        angka+= 5;
+        System.out.println(angka);
+    }
+    
+}
