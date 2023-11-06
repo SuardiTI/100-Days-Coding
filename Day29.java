266d3b79d2e17aa0f9fa7c5ecf3e4b748ad8770b
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-06 23:19:45 +0800

    Belajar Operator perbandingan dan operator logika

---
+++ Day29.java
@@ -0,0 +1,23 @@
+package Day29;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //operator perbandingan
+        int nilai = 8;
+        int angka = 6;
+        System.out.println(nilai == angka);
+        System.out.println(nilai != angka);
+        System.out.println(nilai > angka);
+        System.out.println(angka <= nilai);
+        
+        //operator logika
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a && b);
+        System.out.println(a || b);
+        System.out.println(!a);
+        System.out.println(!b);
+    }
+    
+}
