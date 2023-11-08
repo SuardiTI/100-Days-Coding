4a88aa420f3e5114467f4bbf9db0b7120dbfb7fc
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-08 22:33:00 +0800

    Belajar Perulangan
    Menampilkan angka berurut dari angka terbesar ke terkecil

---
+++ Day31.java
@@ -0,0 +1,13 @@
+package Day31;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //perulangan berurut dari angka terbesar ke angka terkecil
+        for (int a=100; a>=1; a--){
+            System.out.println(a);
+        }
+        
+    }
+    
+}
