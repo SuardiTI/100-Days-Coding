ffe403972130d0a9261656b096b6806170d6b8d9
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-10 21:13:37 +0800

    Belajar Perulangan While

---
+++ Day33.java
@@ -0,0 +1,17 @@
+package Day33;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //Belajar Perulangan While
+        
+        int x = 1;
+        
+        while (x <= 100){
+            System.out.println("Nilai i : "+x);
+            x++;
+        }
+    }
+    
+}
