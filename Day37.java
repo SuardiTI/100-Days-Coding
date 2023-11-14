11c9d43c5bdb74248ba48b776c990e121582d123
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-14 23:41:04 +0800

    Infinite Loop
    Infinite for, mencetak "Saya Sedang Belajar Java" secara tak terbatas.

---
+++ Day37.java
@@ -0,0 +1,11 @@
+package Day37;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        for(;;){
+            System.out.println("Saya Sedang Belajar Java");
+        }
+    }
+    
+}
