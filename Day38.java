c85eb73a2ad0ed1adc7939247e116fc51da7c71d
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-15 22:32:07 +0800

    Infinite Loop
    Infinite While mencetak output secara berulang dan tak terbatas

---
+++ Day38.java
@@ -0,0 +1,12 @@
+package Day38;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Saya Sedang Belajar Java");
+        }
+        
+    }
+    
+}
