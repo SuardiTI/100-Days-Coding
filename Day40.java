9d6a56e3a9010a2445890beeb6b271882cd0d596
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-17 19:51:29 +0800

    Continue

---
+++ Day40.java
@@ -0,0 +1,16 @@
+package Day40;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //continue
+        
+        for(int i=1; i < 50; i++){
+            if(i == 30){ //program akan dilanjutkan ke literasi selanjutnya ketika nilai i sama dengan 30
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+    
+}
