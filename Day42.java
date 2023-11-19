ad5bfb7c094710b2f043586114ba6579090db905
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-19 21:28:23 +0800

    Deklarasi Array

---
+++ Day42.java
@@ -0,0 +1,10 @@
+package Day42;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String[]nama = {"Andi", "Rudi", "Michael"};
+        
+        System.out.println(nama[2]); 
+    }
+}
