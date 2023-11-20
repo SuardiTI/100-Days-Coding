53f40ba18c06534ee58895b4c1a56cb38bf99a52
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-20 21:13:54 +0800

    Deklarasi array bertipe primitif

---
+++ Day43.java
@@ -0,0 +1,12 @@
+package Day43;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //deklarasi array bertipe primitif
+        int[]angka = {10, 11, 12, 13, 14, 15};
+        
+        System.out.println(angka[4]);
+        
+    }
+}
