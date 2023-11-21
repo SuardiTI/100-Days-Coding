7bfb51d47f386b796377992bc50b7d37ad4e8b65
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-21 22:19:34 +0800

    Deklarasi array bertipe primitif

---
+++ Day44.java
@@ -0,0 +1,12 @@
+package Day41;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //deklarasi array bertipe primitif
+        double[]angka = {7.2, 4.1, 6.6, 5.5};
+        
+        System.out.println(angka[2]);
+        
+    }
+}
