083a9b8cffc73f1a9ceb585f6100c297d930a938
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-24 21:44:04 +0800

    Deklarasi array bertipe non-primitif (String)

---
+++ Day47.java
@@ -0,0 +1,15 @@
+package Day47;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //deklarasi array bertipe non-primitif (String)
+        
+        String[]nama = {"Asep", "Budi", "Yanto", "Herman"};
+        
+        System.out.print("Nama anak kedua adalah "+nama[1]);
+        
+        
+    }
+    
+}
