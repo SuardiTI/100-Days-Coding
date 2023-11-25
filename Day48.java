1a4592156e1ad1aada6c7360c6042db765453c0a
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-25 23:00:02 +0800

    Menampilkan item dengan perulangan

---
+++ Day48.java
@@ -0,0 +1,14 @@
+package Day46;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int[]angka = {1, 2, 3 ,4 ,5 ,6};
+        
+        for (int a = 0; a<angka.length; a++){
+            System.out.println(angka[a]);
+        }
+    }
+    
+}
