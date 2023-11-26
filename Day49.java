0d72f4dc5212b1c0e0eb72f03f4661070e83bb64
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-26 20:29:10 +0800

    Menampilkan item dengan perulangan

---
+++ Day49.java
@@ -0,0 +1,14 @@
+package Day46;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String[]nama = {"Suardi", "Michael", "Aco", "Budi"};
+        
+        for (int a = 0; a<nama.length; a++){
+            System.out.println(nama[a]);
+        }
+    }
+    
+}
