7f8a595519c890bb5fa66302b223bcb1b270ae8d
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-11-29 23:14:32 +0800

    Index
    Penggunaan index untuk mengubah nilai pada elemen tertentu

---
+++ Day52.java
@@ -0,0 +1,21 @@
+package Day52;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String [] nama = {"Suardi", "Michael", "Aco", "Budi", "Herman"};
+        
+        System.out.println("Elemen Pada Index 0 : "+nama[0]);
+        System.out.println("Elemen Pada Index 1 : "+nama[1]);
+        System.out.println("Elemen Pada Index 2 : "+nama[2]);
+        System.out.println("Elemen Pada Index 3 : "+nama[3]);
+        System.out.println("Elemen Pada Index 4 : "+nama[4]);
+        
+        nama[2] = "Cipung";
+        System.out.println("Elemen Pada Index 2 Setelah Mengalami Perubahan : "+nama[2]);
+        
+        
+    }
+    
+}
