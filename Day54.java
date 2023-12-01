0d6235cbbbe02a35d85afb4d685baa133b71d880
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-12-01 21:51:48 +0800

    Tampilkan item dengan perulangan
    Menampilkan item dengan menggunakan for(String ... : ...)

---
+++ Day54.java
@@ -0,0 +1,14 @@
+package Day54;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String[]peralatan = {"Gergaji", "Palu", "Pisau", "Gergaji Mesin"};
+        
+        for(String alat  : peralatan){
+            System.out.println(alat);
+        }
+    }
+    
+}
