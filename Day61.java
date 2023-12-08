6e76d2fcdd3e1ec2515ba0f6d4ba7299ae97588e
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-12-08 22:12:29 +0800

    Membuat Fungsi

---
+++ Day61.java
@@ -0,0 +1,15 @@
+package Day61;
+
+public class Main {
+    //fungsi untuk menjumlah
+    public static int penjumlahan(int x, int y){
+        return x + y;
+    }
+    
+    public static void main(String[] args) {
+        //memanggil fungsi penjumlahan
+        int hasilJumlah = penjumlahan(5, 6);
+        System.out.println("Hasil Penjumlahan : "+hasilJumlah);
+    }
+    
+}
