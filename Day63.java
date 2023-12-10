34c06f87691eafb9f8b3c573896206066705c700
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-12-10 18:48:44 +0800

    Fungsi Void
    Mencetak Nama

---
+++ Day63.java
@@ -0,0 +1,15 @@
+package Day63;
+
+public class Main{
+
+    public static void main(String[]args){
+        
+        Main nama = new Main();
+        
+        nama.NamaSaya("Suardi");
+    }
+    public void NamaSaya(String nama){
+        System.out.print("Hello "+nama+"!");
+    }
+        
+}
