88b847eac465e0863d014e44ad8adf2a5ca8c672
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-12-12 22:55:40 +0800

    Fungsi dengan return value dan parameter bertipe data primitif

---
+++ Day65.java
@@ -0,0 +1,15 @@
+package Day65;
+
+public class Main{
+    public static int kali(int angka1, int angka2){
+        int hasil = angka1*angka2;
+        return hasil;
+    }
+    public static void main(String[]args){
+        int hasilKali = kali(7, 7);
+        
+        System.out.print("Hasil Perkalian : "+hasilKali);
+        
+    }
+        
+}
