6f30adb3dcb8da83a2e14f4f7aff9743f8db7640
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-12-14 21:26:26 +0800

    Memanggil Fungsi

---
+++ Day67.java
@@ -0,0 +1,16 @@
+package Day67;
+
+public class Main{
+    public static void namaDepan(){
+        System.out.print("Agus ");
+    }
+    public static void namaBelakang(){
+        System.out.print("Setiawan");
+    }
+    public static void main(String[]args){
+        //memanggil fungsi
+        namaDepan();
+        namaBelakang();
+    }
+        
+}
