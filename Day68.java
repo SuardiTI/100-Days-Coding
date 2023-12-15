af001fafc6d1104d6b5432166820894fe497b3e3
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-12-15 22:21:09 +0800

    String charAt( )

---
+++ Day68.java
@@ -0,0 +1,14 @@
+package Day68;
+
+public class Main{
+    
+    public static void main(String[]args){
+        String nama = "SUARDI";
+        
+        char karakter = nama.charAt(3);
+        
+        System.out.print("Karakter Pada Index 3 : "+karakter);
+        
+    }
+        
+}
