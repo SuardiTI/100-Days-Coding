e2afc05ef7f7e822de6349fdf08a7e888458d472
SuardiTI <147118104+SuardiTI@users.noreply.github.com>
2023-12-20 00:52:52 +0800

    String Concatenation
    Menggunakan String Concatenation untuk menggabungkan nama depan dan nama belakang

---
+++ Day73.java
@@ -0,0 +1,16 @@
+package Day73;
+
+public class Main{
+    
+    public static void main(String[]args){
+        //Concatenation/penggabungan
+      
+        String namaDepan = "Kurnia";
+        String namaBelakang = "Pratiwi";
+        
+        // Menggabungkan string dengan metode concat()
+        String hasilPenggabungan = namaDepan.concat(" ").concat(namaBelakang);
+        System.out.println(hasilPenggabungan);
+           
+     }
+}
